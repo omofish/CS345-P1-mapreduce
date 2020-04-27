a0581fdffe7e956a60ec3e7b4588443f5acc559c
omofish <jason_lzx@hotmail.com>
2020-04-27 23:22:12 +0800

    scheduled implemented, not tested

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -1,6 +1,9 @@
 package mapreduce
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
 //
 // schedule() starts and waits for all tasks in the given phase (mapPhase
@@ -22,6 +25,65 @@ func schedule(jobName string, mapFiles []string, nReduce int, phase jobPhase, re
 		ntasks = nReduce
 		n_other = len(mapFiles)
 	}
+
+	// IMPT: All code written here was not tested because it does
+	// not run properly on Windows. Will get a server from NU asap
+	// All this rn is just theory on the correct code but... no guarantees lol
+	// Start of Jason's code
+
+	// create a waitgroup to wait for all goroutines to finish
+	var wg sync.WaitGroup
+
+	// for each task in task list
+	for i := 0; i < ntasks; i++ {
+		// get current worker's RPC. Blocks if no workers in channel
+		rpc := <-registerChan
+
+		// Increment wait group by 1 for new rpc added
+		wg.Add(1)
+
+		// create new DoTaskArgs for current worker
+		args := DoTaskArgs{jobName, mapFiles[i], phase, i, n_other}
+
+		// call() sends an RPC to the rpcname handler on server srv
+		// with arguments args, waits for the reply, and leaves the
+		// reply in reply. the reply argument should be the address
+		// of a reply structure.
+
+		// call() returns true if the server responded, and false if call()
+		// received no reply from the server. reply's contents are valid if
+		// and only if call() returned true.
+
+		// go for concurrency
+		go func() {
+		loop:
+			for {
+				// IMPT: reply is currently assigned to a string primitive
+				reply := ""
+				ok := call(rpc, "Worker.DoTask", args, &reply)
+				if ok == false {
+					// if worker doesn't reply, goes back to loop
+					fmt.Printf("Worker %s did not reply. Resending\n", rpc)
+
+				} else {
+					// decrease wg counter if reply received
+					wg.Done()
+
+					// send worker back onto registerChan so we can
+					// give it another task (Not sure if this is legit)
+					registerChan <- rpc
+
+					//loop breaks if valid reply received
+					break loop
+				}
+			}
+		}()
+	}
+
+	// Block until waitgroup counter goes to 0: all workers are finished
+	wg.Wait()
+
+	// End of Jason's Code
 
 	fmt.Printf("Schedule: %v %v tasks (%d I/Os)\n", ntasks, phase, n_other)
 
